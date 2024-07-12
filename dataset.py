584a8aeee5ad50e50279a21c03b64dcf8706c121
rishabh negi <ronynegi676@gmail.com>
2024-07-12 11:18:03 +0530

    modified dataset code

--- dataset.py
+++ dataset.py
@@ -16,7 +16,9 @@ def face_extractor(img):
     
     cropped_faces = []
     for (x, y, w, h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
         cropped_face = img[y:y+h, x:x+w]
+        
         cropped_faces.append(cropped_face)
         
     return cropped_faces
@@ -32,18 +34,18 @@ try:
         if faces:
             for face in faces:
                 count += 1
-                face = cv2.resize(face, (200, 200))
+                face = cv2.resize(face, (400, 400))
                 face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
                 
                 file_name_path = os.path.join(path, sub_dataset + '_' + str(count) + '.jpg')
                 cv2.imwrite(file_name_path, face)
                 
                 cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                cv2.imshow('Face Cropper', face)
+                cv2.imshow('Face Cropper', frame)
         else:
             print("Face not found")
         
-        if cv2.waitKey(1) == 13 or count == 100:
+        if cv2.waitKey(1) == 27 or count == 100:
             break
 
 except KeyboardInterrupt:
